6834788d84795873dff114de2de998db87c23d58
Whil Piavis <ncsupiratecaptain@gmail.com>
2020-01-27 10:41:20 -0700

    Exercise 12-1

---
+++ 12ex1.rb
@@ -0,0 +1,16 @@
+# Seeing the MRI Garbage Collection in Action.
+
+def display_count
+  # ObjectSpace - information on GC allocaiton space and current object.s
+  data = ObjectSpace.count_objects
+  puts "Total: #{data[:TOTAL]} Free: #{data[:FREE]} Object: #{data[:T_OBJECT]}"
+end
+
+30.times do
+  obj = Object.new
+  display_count
+end
+
+# Full garbage collection!
+GC.start
+display_count
